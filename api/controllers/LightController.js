96de4d6e0fdf44e9ada4f9ecc3909c2ddbdabc05
Doug Estey <doug.estey@gmail.com>
2018-05-13 11:28:55 -0400

    api improvements

--- api/controllers/LightController.js
+++ api/controllers/LightController.js
@@ -14,68 +14,67 @@ module.exports = {
 
   async allOn(req, res) {
     let lights = await Huejay.lights.getAll();
+    lights = lights.map((light) => light.reachable);
 
     for (let light of lights) {
       light.on = true;
       light.brightness = 100;
       light.saturation = 100;
       light.colorTemp = 389;
+      light.transitionTime = 3;
 
       await Huejay.lights.save(light);
     }
 
-    return res.status(200).json({ message: 'Lights are on. '});
+    return res.status(200).json({ message: 'Lights are on.' });
   },
 
   async allOff(req, res) {
     let lights = await Huejay.lights.getAll();
+    lights = lights.map((light) => light.reachable);
 
     for (let light of lights) {
       light.on = false;
+      light.transitionTime = 3;
+
       await Huejay.lights.save(light);
     }
 
-    return res.status(200).json({ message: 'Lights are off. '});
+    return res.status(200).json({ message: 'Lights are off.' });
   },
 
   async allFire(req, res) {
     let rndm = (m) => { return Math.floor(Math.random() * m) + 1; };
-
     let lights = await Huejay.lights.getAll();
+    lights = lights.map((light) => light.reachable);
 
     let run = async() => {
       for (let light of lights) {
         light.on = true;
         light.hue = rndm(8000);
         light.brightness = rndm(60);
+        light.transitionTime = 0.5;
   
-        try {
-          await Huejay.lights.save(light);
-        } catch (e) {
-          console.log(e);
-        }
+        await Huejay.lights.save(light);
       }
     };
 
     setInterval(run, 10000);
 
-    return res.status(200).json({ message: 'Lights are off. '});
+    return res.status(200).json({ message: 'Lights are on fire.' });
   },
 
   async waves(req, res) {
-    // let rndm = (m) => { return Math.floor(Math.random() * m) + 1; };
-
     let lights = await Huejay.lights.getAll();
+    lights = lights.map((light) => light.reachable);
 
-    // let run = async() => {
     let run = async(light) => {
       light.on = true;
       light.hue = 45000;
       light.brightness = 20;
       light.saturation = 100;
-      // light.colorTemp = 500;
   
-     Huejay.lights.save(light);
+      await Huejay.lights.save(light);
 
       setTimeout(() => {
         light.on = false;
@@ -83,28 +82,11 @@ module.exports = {
       }, 250);
     }
   
-    // let delay = 0;
-
-    // for (let light of lights) {
-      // delay = delay + 1000;
     setTimeout(() => run(lights[0]), 1000);
     setTimeout(() => run(lights[3]), 2000);
     setTimeout(() => run(lights[4]), 3000);
-    // }
-    // }
 
-    // for (var i; i < lights.length; i++) {
-    //   let light = lights[i];
-    //   console.log(light);
-
-    //   setTimeout(() => {
-    //     run(light);
-    //   }, 1000);
-    // }
-
-    // setInterval(run, 5000);
-
-    return res.status(200).json({ message: 'Lights are wavy. '});
+    return res.status(200).json({ message: 'Lights are wavy.' });
   }
 
 };
--- api/controllers/SensorController.js
+++ api/controllers/SensorController.js
@@ -4,7 +4,7 @@ module.exports = {
     let value = Sensors.IRAlpha.read();
 
     if (value)
-      return res.status(200)
+      return res.status(200).json({ message: `Success`, value })
     else
       return res.status(400).json({ message: `Didn't receive anything from the sensor in time. `});
   }
