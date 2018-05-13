be8083cfe2cd5ce897f6803448f718cc24480ca9
Doug Estey <doug.estey@gmail.com>
2018-05-13 12:47:25 -0400

    polling

--- api/controllers/SensorController.js
+++ api/controllers/SensorController.js
@@ -5,6 +5,20 @@ module.exports = {
     let value = Sensors.IRAlpha.read(pin);
 
     return res.status(200).json({ message: `Success`, value, pin });
+  },
+
+  start(req, res) {
+    let pin = parseInt(req.params.pin);
+    Sensors.IRAlpha.start(pin);
+
+    return res.status(200).json({ message: `Sensor is watching for humans.` });
+  },
+
+  stop(req, res) {
+    let pin = parseInt(req.params.pin);
+    Sensors.IRAlpha.stop(pin);
+
+    return res.status(200).json({ message: `Sensor is going to sleep.` });
   }
 
 };
