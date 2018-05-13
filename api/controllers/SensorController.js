de5c986756239436eeeb64be25163a3120c8c13e
Doug Estey <doug.estey@gmail.com>
2018-05-13 11:30:14 -0400

    api improvements

--- api/controllers/SensorController.js
+++ api/controllers/SensorController.js
@@ -1,10 +1,11 @@
 module.exports = {
 
   read(req, res) {
+    let pin = Sensors.IRAlpha.pin;
     let value = Sensors.IRAlpha.read();
 
     if (value)
-      return res.status(200).json({ message: `Success`, value })
+      return res.status(200).json({ message: `Success`, value, pin })
     else
       return res.status(400).json({ message: `Didn't receive anything from the sensor in time. `});
   }
